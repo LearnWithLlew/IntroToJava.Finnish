a5eb98204eda4eecea0311ea825609cf7b7892c5
Llewellyn Falco <isidore@setgame.com>
2016-03-24 13:33:05 +0200

    Moved from TKP after lynn stole it from me

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
@@ -0,0 +1,18 @@
+package org.teachingkidsprogramming.section01forloops;
+
+public class SimpleSquare
+{
+  public static void main(String[] args) throws Exception
+  {
+    //  Näytä kilpikonna --#1
+    //  Laita kilpikonna liikkumaan mahdollisimman nopeasti --#6
+    //  Toista seuraava 4 kertaa --#5.1
+    //      Muuta kilpikonnan piirtämän viivan väriksi "sininen" --#4
+    //      Siirrä kilpikonnaa 50 pikseliä --#2
+    //      Käännä kilpikonnaa oikealle (90 astetta) --#3
+    //  Toista --#5.2
+  }
+  /*
+   * After you finish, watch the video recap at http://youtu.be/5bqUZEsJh_Q
+   */
+}
